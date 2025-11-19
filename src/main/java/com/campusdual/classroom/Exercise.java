57e6f504be441e2cb5e9b19b70e35c5340a16957
Mario Rodríguez Martínez <mario.rodriguez.martinez.lu@gmail.com>
2025-11-19 09:51:05 +0100

    Solved exercise 28

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,7 +1,13 @@
 package com.campusdual.classroom;
 
 public class Exercise {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+            try {
+                throw new Exception("Esto es una Excepción");
+            } catch (Exception e) {
+                System.out.println("Excepción controlada: " + e.getMessage());
+            }
 
+            System.out.println("Programa finalizado");
+        }
     }
-}
